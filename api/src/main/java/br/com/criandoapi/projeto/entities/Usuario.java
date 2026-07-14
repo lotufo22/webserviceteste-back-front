071451b5744d8e8e0e0ac06b4b18b852db526fc0
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-07-13 21:00:10 -0300

    refactor: replace getters, setters and constructors with Lombok

--- api/src/main/java/br/com/criandoapi/projeto/entities/Usuario.java
+++ api/src/main/java/br/com/criandoapi/projeto/entities/Usuario.java
@@ -6,6 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
+// Annotation de Lombok para os métodos especiais da classe
+@Data
 
 @Entity
 @Table(name="usuario")
@@ -27,58 +31,4 @@ public class Usuario {
 	
 	@Column(name = "telefone", length = 15, nullable = true)
 	private String telefone;
-	
-	public Usuario() {
-	}
-
-	public Usuario(Integer id, String nome, String email, String senha, String telefone) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-		this.telefone = telefone;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	
-	
 }
