8a9cdcb02f3e46b105da353415e519f4a64f62f8
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-07-15 00:10:22 -0300

    feat: implement JWT token generation and validation service

--- api/src/main/java/br/com/criandoapi/projeto/security/SecurityConfig.java
+++ api/src/main/java/br/com/criandoapi/projeto/security/SecurityConfig.java
@@ -2,30 +2,31 @@ package br.com.criandoapi.projeto.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
-
-        return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSec) throws Exception {
+		httpSec
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(HttpMethod.GET, "/usuarios").permitAll()
+				.anyRequest().authenticated()
+			)
+			.cors(Customizer.withDefaults());
+		
+		httpSec.addFilterBefore(
+				new SecurityFilter(),
+				UsernamePasswordAuthenticationFilter.class);
+		
+		return httpSec.build();
+	}
 }
